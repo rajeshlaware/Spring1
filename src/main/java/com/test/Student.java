fb4efde380e3781ce2b4e53c26e8e9803cdb454d
rajesh <rajlaware2012@gmail.com>
2023-02-10 00:18:51 +0530

    added the spring1 project

---
+++ src/main/java/com/test/Student.java
@@ -0,0 +1,39 @@
+package com.test;
+
+public class Student {
+	
+	private String firstName;
+	private int age;
+	private String city;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "test [firstName=" + firstName + ", age=" + age + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+
+}
